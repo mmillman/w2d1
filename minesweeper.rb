f676ab7c000c8c6cf1318adf4d43db3a9132bca6
Mark Millman and Peter Lin <peter+mark@appacademy.io>
2013-03-18 11:08:09 -0700

    Commit board backed by flat array

--- minesweeper.rb
+++ minesweeper.rb
@@ -18,8 +18,31 @@ end
 
 class Board
 
-	def initialize(row, cols, mines)
+	def initialize(num_rows, num_cols, num_mines)
+		@num_rows, @num_cols = num_rows, num_cols
+		@blocks = Array.new(num_rows * num_cols) { :unexplored }
+		num_mines.times { |index| @blocks[index] = :mine }
+		@blocks.shuffle
+		add_fringe
+		@flags = {}
+	end
 
+	def add_fringe
+		@blocks.each_with_index do |block, index|
+			if block == :mine
+				p index
+			end
+		end
+	end
+
+	def at(x, y)
+
+	end
+
+	def to_s
+		@blocks.map do |block|
+
+		end
 	end
 
 end
